eafd5394d9ca37c11512ea9fcd0b72f12dc707ac
boot camp <bootcamp@Rons-MacBook-Pro.local>
2016-04-23 14:55:27 -0400

    modification2

--- app/search/search.js
+++ app/search/search.js
@@ -12,6 +12,19 @@ angular.module('myApp.search', ['ngRoute'])
         });
     }])
 
-    .controller('searchCtrl', [function() {
+    .controller('searchCtrl', ['$scope', '$http', function($scope,$http) {
+        $scope.search = function (){
+            var searchUrl='https://api.spotify.com/v1/search?q=' + $scope.searchValue + '&type='+ $scope.searchType;
+            $http.get(searchUrl).then(setScope)
+        }
+        var setScope = function(response) {
+            var searchType = $scope.searchType;
+            $scope.searchResults = response.data[searchType + 's'].items;
+        }
+        
+        $scope.searchType = 'artist';
+        $scope.changeRadio = function() {
+            $scope.searchResults = null;
+        }
 
     }]);
